7ff4b8b4d1a90b06eeabea725a49e312a606994d
Aaron Hakala <aaron.hakala@gmail.com>
2020-03-16 17:10:46 +0100

    messing around

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,16 +2,25 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.[tj]s"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-  ],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: async (config, { configType }) => {
     config.module.rules = config.module.rules.filter(f => {
       if (f.test) return f.test.toString() !== "/\\.css$/";
       return true;
     });
+
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve("ts-loader"),
+          options: {
+            reportFiles: ["src/**/*.{ts|tsx}"],
+          },
+        },
+      ],
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
 
     config.module.rules.push({
       test: /\.css$/,
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,12 @@ module.exports = {
     siteUrl: `https://www.aaronhakala.com`,
     description: `Software dev in the modern web.`,
   },
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-robots-txt",
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+  ],
 };
